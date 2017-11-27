1cb5b00aa21cfcc1eb66c2b5da2faee2776ab6cc
User <user@Apple-Macbook-Air.local>
2017-11-27 17:21:27 +0200

    first commit

---
+++ models.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+module.exports = function(mongoUrl) {
+    mongoose.connect(mongoUrl, function(err, results) {
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log("connected to database")
+    }
+    });
+
+
+    var plumbersShifts = mongoose.model('plumbersShifts', {
+        name: String,
+        days: Object
+
+
+    });
+
+
+    return {
+      plumbersShifts
+    }
+}
