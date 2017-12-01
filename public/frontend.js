b499eb745758ea2c758a52791dd60cf9380d1ea3
bootcamp <makhosandilezukelwa@gmail.com>
2017-12-01 15:08:14 +0200

    new commit

---
+++ public/frontend.js
@@ -0,0 +1,5 @@
+var addButton = document.querySelector(".addButton");
+var nameInput = document.querySelector(".nameClass");
+var emailInput = document.querySelector(".emailClass");
+var sizeInput = document.querySelector(".contactClass");
+var rr = document.querySelector(".rr")
